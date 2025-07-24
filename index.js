c6e691ba02bb25aae8ef4b2980f2f6e3fae58bea
David Perez <davidperez.au@gmail.com>
2025-07-24 19:17:58 +1000

    Grid working better now

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const numOfDivs = 16;
 const containerMain = document.getElementById('mainContainer');
 
 function createDivs(num){
-    for(let i = 0; i < num; i++){
+    for(let row = 0; row < num; row++){
         const newDiv = document.createElement("div");
         newDiv.className = "div";
         containerMain.appendChild(newDiv);
 
-        for(let i=0; i < num; i++){
+        for(let col = 0; col < num; col++){
             const square = document.createElement("div");
             newDiv.appendChild(square);
             square.className = "square";
@@ -17,4 +17,4 @@ function createDivs(num){
 }
 
 createDivs(numOfDivs);
-ß
+
