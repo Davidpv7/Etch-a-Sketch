30b1f7dcb552ea1933d11967cc004cc9284ffff4
David Perez <davidperez.au@gmail.com>
2025-07-25 07:56:43 +1000

    project ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const numOfDivs = 16;
-
 const containerMain = document.getElementById('mainContainer');
+const resizeBtn = document.getElementById('resizeBtn')
+
 
 function createDivs(num){
     for(let row = 0; row < num; row++){
@@ -12,9 +13,24 @@ function createDivs(num){
             const square = document.createElement("div");
             newDiv.appendChild(square);
             square.className = "square";
+            square.addEventListener("mouseover", () => {
+                square.classList.add('hovered');
+            })
         }
     }
 }
+
+resizeBtn.addEventListener("click", () => {
+    let userInput = prompt("Enter grid size");
+    let newSize = parseInt(userInput);
+    if (isNaN(newSize) || newSize < 1  || newSize > 100){
+        alert("Please enter a valid number between 1 and 100");
+        return;
+    }
+    containerMain.innerHTML = '';
+    createDivs(newSize)
+});
+
 
 createDivs(numOfDivs);
 
